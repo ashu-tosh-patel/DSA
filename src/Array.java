44a599d91d1418726e19283756325ac650ef497b
aniket patel <aaniket8840@gmail.com>
2022-10-07 19:38:01 +0530

    added a fxn to print the array

--- src/Array.java
+++ src/Array.java
@@ -1,9 +1,14 @@
 public class Array {
-    int[] prefixSum(int a[]){
+    int[] prefixSum(int a[]) {
         int[] psum = new int[a.length];
         psum[0] = a[0];
-        for(int i =1; i< a.length; i++)
-            psum[i] = psum[i-1]+a[i];
+        for (int i = 1; i < a.length; i++)
+            psum[i] = psum[i - 1] + a[i];
         return psum;
+    }
+
+    void printArray(int[] a) {
+        for (int i : a)
+            System.out.print(i + " ");
     }
 }
