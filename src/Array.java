96874d61a3dcfc1ad8830beebc37798393b540fe
Abhishek Patel <patelabhishek2007@gmail.com>
2022-10-07 10:24:21 +0530

    Update Array.java

--- src/Array.java
+++ src/Array.java
@@ -1,10 +1,9 @@
 public class Array {
     int[] prefixSum(int a[]){
-        int ps[] = new int[a.length];
-        ps[0] = a[0];
-        for(int i =1; i<a.length; i++){
-            ps[i] = a[i]+ps[i-1];
-        }
-        return ps;
+        int[] psum = new int[a.length];
+        psum[0] = a[0];
+        for(int i =1; i< a.length; i++)
+            psum[i] = psum[i-1]+a[i];
+        return psum;
     }
 }
